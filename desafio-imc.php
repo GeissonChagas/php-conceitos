356bf0a344e889f303ac9a44e109e4a6de9c88fa
GeissonChagas <geisson.excel@gmail.com>
2023-04-02 19:13:24 -0300

    implementando classificações no imc

--- desafio-imc.php
+++ desafio-imc.php
@@ -4,13 +4,23 @@ echo "## Este desafio consiste em fazer o cálculo de IMC:" . PHP_EOL . PHP_EOL;
 // sabendo que o cálculo de IMC é feito através da seguinte fórmula: peso/altura^2
 
 // digite aqui o peso em (kg):
-$peso = 85;
+$peso = 60;
 
 //digite aqui a altura em (m):
 $altura = 1.80;
 
 $imc = ($peso/($altura ** 2));
 
-echo "O IMC de uma pessoa com $peso kg e $altura m de altura é $imc";
+echo "O IMC de uma pessoa com $peso kg e $altura m de altura é $imc e está ";
+
+if ($imc < 18.5){
+    echo "abaixo do recomendado!";
+} elseif ($imc < 25 ) {
+    echo "ideal!";
+} else {
+    echo "acima do recomendado!";
+}
+
+
 
 ?>
